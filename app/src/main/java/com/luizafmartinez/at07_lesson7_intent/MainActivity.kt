0b28dd3d74711810b610ec4d80bbab2cc48a5204
alxmart <alxmartinez@gmail.com>
2024-03-31 17:29:24 -0300

    Added viewBinding configurations.

--- app/src/main/java/com/luizafmartinez/at07_lesson7_intent/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/at07_lesson7_intent/MainActivity.kt
@@ -5,15 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luizafmartinez.at07_lesson7_intent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
